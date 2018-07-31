eeb2416afd72385e044d36b6873cacaddffb32ea
KavenC <kavenc@gmail.com>
2018-08-01 00:57:30 +0800

    Fix webhook regex

--- internal/pkg/telepathy/httpserver.go
+++ internal/pkg/telepathy/httpserver.go
@@ -15,12 +15,12 @@ func init() {
 }
 
 // RegisterWebhook is used to register a http callback, like webhooks
-// The pattern can only be in this regular expression format: ^[A-Ba-b](-[A-Ba-b0-9]){0. 3}$, otherwise it is ignored.
+// The pattern can only be in this regular expression format: ^[A-Za-z]+(-[A-Za-z0-9]+){0,3}$, otherwise it is ignored.
 // If the pattern is already registered, it panics.
 // Webhooks are always registered at (host)/webhook/<patter>
 func RegisterWebhook(pattern string, handler func(http.ResponseWriter, *http.Request)) {
 	if valid == nil {
-		valid = regexp.MustCompile(`^[A-Ba-b](-[A-Ba-b0-9]){0. 3}$`)
+		valid = regexp.MustCompile(`^[A-Za-z]+(-[A-Za-z0-9]+){0,3}$`)
 	}
 	if valid.MatchString(pattern) {
 		logrus.Info("Registering webhook pattern: " + pattern)

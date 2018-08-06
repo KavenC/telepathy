4d06cec6e40f7e6f02dffb300ef754b29e0237f1
KavenC <kavenc@gmail.com>
2018-08-06 19:43:47 +0800

    Implement user new command

--- internal/pkg/telepathy/cmduser.go
+++ internal/pkg/telepathy/cmduser.go
@@ -2,6 +2,7 @@ package telepathy
 
 import (
 	"github.com/KavenC/cobra"
+	"github.com/sirupsen/logrus"
 )
 
 func init() {
@@ -22,5 +23,14 @@ func init() {
 }
 
 func newCmdHandle(cmd *cobra.Command, args []string, extras ...interface{}) {
-	cmd.Print("Hi User.")
+	message, ok := extras[0].(*InboundMessage)
+	if !ok {
+		logger := logrus.WithField("command", args)
+		logger.Errorf("Invalid argument type in command handlers: %T", extras[0])
+		panic("Invalid argument type in command handlers")
+	}
+
+	if !message.IsDirectMessage {
+		cmd.Print("This command can only be run with Direct Messages (Whispers).")
+	}
 }
